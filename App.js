617ada904624e59c2d138291e07231830fa1bc77
Carlos Alvarado <cjclonjedi@gmail.com>
2020-02-11 16:51:54 -0400

    Fixed Text.

--- App.js
+++ App.js
@@ -4,11 +4,11 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello There!</Text>
-      <Text>                       -Obi wan Kenobi</Text>
       <Image source = {{uri:'https://seeklogo.com/images/M/msi-logo-47BD237377-seeklogo.com.png'}}
       style = {{ width: 243, height: 300 }}
       />
+      <Text></Text>
+      <Text style={{color: '#fafafa'}} >Welcome to the MSI Team</Text>
     </View>
   );
 }
