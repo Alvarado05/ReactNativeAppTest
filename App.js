b24a7ddc4e5d8e2f9004265e9fa99b8113be3ddb
Carlos Alvarado <cjclonjedi@gmail.com>
2020-02-11 16:34:59 -0400

    Created the React Native App

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello There!</Text>
+      <Text>                       -Obi wan Kenobi</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
