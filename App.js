382cf38d1f074c62a285792bf3363087edc1e8fa
Carlos Alvarado <cjclonjedi@gmail.com>
2020-02-11 17:06:27 -0400

    Added a second view and made the deafult view to a firstView

--- App.js
+++ App.js
@@ -3,13 +3,7 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Image source = {{uri:'https://seeklogo.com/images/M/msi-logo-47BD237377-seeklogo.com.png'}}
-      style = {{ width: 243, height: 300 }}
-      />
-      <Text></Text>
-      <Text style={{color: '#fafafa'}} >Welcome to the MSI Team</Text>
-    </View>
+    firstView
   );
 }
 
@@ -21,3 +15,24 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const firstView = (
+  <View style={styles.container}>
+    <Image source = {{uri:'https://seeklogo.com/images/M/msi-logo-47BD237377-seeklogo.com.png'}}
+    style = {{ width: 243, height: 300 }}
+    />
+    <Text></Text>
+    <Text style={{color: '#fafafa'}} >Welcome to the MSI Team</Text>
+  </View>
+)
+
+const secondView = (
+  <View style={styles.container}>
+    <Text style={{fontWeight:'bold'}}>
+      Text Bold
+        <Text style={{color:'red'}}>
+          Nested Text in red
+        </Text>
+    </Text>
+  </View>
+)
